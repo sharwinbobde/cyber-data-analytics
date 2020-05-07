794d74a5a9abf2f1b25293edac631b5665d4aa29
sharwinbobde <bsharwin@gmail.com>
2020-05-07 17:01:57 +0200

    fixed most things :)

--- Part 1/models.py
+++ Part 1/models.py
@@ -16,14 +16,22 @@ class models:
         result = run_metrics(Y_test, y_pred)
         print("Done")
         return result
-    
+
     @staticmethod
-    def SVM(X_train, X_test, Y_train, Y_test):
-        print("Building SVM")
-        model = SVC()
+    def NaiveBayes(X_train, X_test, Y_train, Y_test):
+        from sklearn.naive_bayes import GaussianNB
+        print("Building Gaussian Naive Bayes model")
+        model = GaussianNB()
         model.fit(X_train, Y_train)
         y_pred = model.predict(X_test)
 
         result = run_metrics(Y_test, y_pred)
         print("Done")
         return result
+
+    @staticmethod
+    def try_all(X_train, X_test, Y_train, Y_test):
+        results = {}
+        results['NaiveBayes'] = models.NaiveBayes(X_train, X_test, Y_train, Y_test)
+        results['DecisionTree'] = models.NaiveBayes(X_train, X_test, Y_train, Y_test)
+        return results
