87b24ed71949a9acfefcf10184c3548c9961f486
Nikhil Saldanha <nikhil.saldanha691@gmail.com>
2020-05-08 13:40:47 +0200

    Add feature extraction to notebook
    update with standard github .gitignore
    
    update aggregate_features.py with new initial values
    
    remove aggregate_features method from preprocessing.py

--- Part 1/aggregate_features.py
+++ Part 1/aggregate_features.py
@@ -26,13 +26,19 @@ def aggregate_features(df):
             week_ago = now - pd.offsets.DateOffset(weeks=1)
             day_ago = now - pd.offsets.DateOffset(days=1)
 
-            prev_month_txns = (transaction_date.between(month_ago, now)) & (same_card_id)
+            prev_month_txns = (transaction_date.between(month_ago, now)) & (
+                same_card_id
+            )
             prev_week_txns = (transaction_date.between(week_ago, now)) & (same_card_id)
             prev_day_txns = (transaction_date.between(day_ago, now)) & (same_card_id)
 
-            prev_month_num_txns = df[prev_month_txns].shape[0]
-            prev_month_num_same_country = df[(prev_month_txns) & (same_country)].shape[0]
-            prev_month_num_same_currency = df[(prev_month_txns) & (same_currency)].shape[0]
+            # prev_month_num_txns = df[prev_month_txns].shape[0]
+            prev_month_num_same_country = df[(prev_month_txns) & (same_country)].shape[
+                0
+            ]
+            prev_month_num_same_currency = df[
+                (prev_month_txns) & (same_currency)
+            ].shape[0]
 
             row["prev_month_avg_amount"] = df.loc[prev_month_txns, "amount_eur"].mean()
             row["prev_week_avg_amount"] = df.loc[prev_week_txns, "amount_eur"].mean()
@@ -44,15 +50,9 @@ def aggregate_features(df):
                 .mean()
                 .to_numpy()[0]
             )
-            row["prev_day_perc_same_country"] = (
-                df[(prev_day_txns) & (same_country)].shape[0] / df[prev_day_txns].shape[0]
-            )
-            row["prev_month_perc_same_country"] = (
-                prev_month_num_same_country / prev_month_num_txns
-            )
-            row["prev_month_perc_same_currency"] = (
-                prev_month_num_same_currency / prev_month_num_txns
-            )
+            row["prev_day_same_country"] = df[(prev_day_txns) & (same_country)].shape[0]
+            row["prev_month_same_country"] = prev_month_num_same_country
+            row["prev_month_same_currency"] = prev_month_num_same_currency
 
             if prev_month_num_same_country == 0.0:
                 row["prev_month_avg_amount_same_country"] = 0.0
@@ -67,17 +67,19 @@ def aggregate_features(df):
                 row["prev_month_avg_amount_same_currency"] = df.loc[
                     (prev_month_txns) & (same_currency), "amount_eur"
                 ].mean()
+            row["prev_total_transactions"] = df.loc[same_card_id].shape[0]
         else:
-            row["prev_month_avg_amount"] = row["amount_eur"]
-            row["prev_week_avg_amount"] = row["amount_eur"]
-            row["prev_day_amount"] = row["amount_eur"]
-            row["daily_avg_over_month"] = row["amount_eur"]
-            row["prev_day_perc_same_country"] = 1.0
-            row["prev_month_perc_same_country"] = 1.0
-            row["prev_month_avg_amount_same_country"] = row["amount_eur"]
-            row["prev_month_perc_same_currency"] = 1.0
-            row["prev_month_avg_amount_same_currency"] = row["amount_eur"]
+            row["prev_month_avg_amount"] = 0.0
+            row["prev_week_avg_amount"] = 0.0
+            row["prev_day_amount"] = 0.0
+            row["daily_avg_over_month"] = 0.0
+            row["prev_day_same_country"] = 0.0
+            row["prev_month_same_country"] = 0.0
+            row["prev_month_avg_amount_same_country"] = 0.0
+            row["prev_month_same_currency"] = 0.0
+            row["prev_month_avg_amount_same_currency"] = 0.0
+            row["prev_total_transactions"] = 0.0
         return row
 
-    df = df.groupby("card_id").apply(agg_fn)
+    df = df.apply(agg_fn, axis=1)
     return df
--- Part 1/preprocessing.py
+++ Part 1/preprocessing.py
@@ -4,25 +4,19 @@ from sklearn.preprocessing import LabelEncoder
 
 
 def relabel(df):
-    df = df.loc[~(df["simple_journal"] == "Refused")]
-    df.loc[df["simple_journal"] == "Chargeback", "simple_journal"] = 1
-    df.loc[df["simple_journal"] == "Settled", "simple_journal"] = 0
-    return df
+    df_relabeled = df.loc[~(df["simple_journal"] == "Refused")]
+    df_relabeled.loc[
+        df_relabeled["simple_journal"] == "Chargeback", "simple_journal"
+    ] = 1
+    df_relabeled.loc[df_relabeled["simple_journal"] == "Settled", "simple_journal"] = 0
+    df_relabeled.simple_journal = df_relabeled.simple_journal.astype("int32")
+    return df_relabeled
 
 
 def encode(df, col, values=None):
     """
-    "card_id",
-    "ip_id",
-    "issuercountrycode",
-    "shoppercountrycode",
-    "txvariantcode",
-    "currencycode",
-    "shopperinteraction",
-    "accountcode",
-    "cardverificationcodesupplied",
-    "mail_id",
-
+    encodes the column passed
+    returns the df and the encoder object
     """
     enc = LabelEncoder()
     if values is None:
@@ -30,7 +24,7 @@ def encode(df, col, values=None):
     else:
         enc.fit(values)
     df[col] = enc.transform(df[col])
-    return df
+    return df, enc
 
 
 def replace_na_with(df, col, replace_value):
@@ -81,59 +75,3 @@ def conv_to_eur(row):
         "SEK": 10.635,
     }
     return row["amount"] / (currency_dict[row["currencycode"]] * 100)
-
-
-def preprocess(df):
-    df = relabel(df)
-
-    df = replace_na_with(df, "cardverificationcodesupplied", False)
-    df = replace_na_with(df, "issuercountrycode", "ZZ")
-    df = replace_na_with(df, "shoppercountrycode", "ZZ")
-
-    df = encode(df, "card_id")
-    df = encode(df, "ip_id")
-    df = encode(df, "txvariantcode")
-    df = encode(df, "shopperinteraction")
-    df = encode(df, "cardverificationcodesupplied")
-
-    df["creationdate"] = pd.to_datetime(df["creationdate"])
-    df["date"] = df["creationdate"].dt.date
-
-    unique_issuer_cc = df["issuercountrycode"].unique().tolist()
-    unique_shopper_cc = df["shoppercountrycode"].unique().tolist()
-    unique_codes = list(set(unique_issuer_cc + unique_shopper_cc))
-    df = encode(df, "issuercountrycode", values=unique_codes)
-    df = encode(df, "shoppercountrycode", values=unique_codes)
-
-    df["amount_eur"] = df.apply(lambda x: conv_to_eur(x), axis=1)
-    df = encode(df, "currencycode")
-
-    df["accountcode"] = df["accountcode"].apply(lambda x: re.sub("Account", "", x))
-    df["accountcode_cc"] = 0
-    df.loc[(df["accountcode"] == "UK"), "accountcode_cc"] = "GB"
-    df.loc[(df["accountcode"] == "Mexico"), "accountcode_cc"] = "MX"
-    df.loc[(df["accountcode"] == "Sweden"), "accountcode_cc"] = "SE"
-    df.loc[(df["accountcode"] == "APAC"), "accountcode_cc"] = "APAC"
-    df = encode(df, "accountcode_cc")
-
-    df.loc[df["mail_id"].str.contains("na", case=False), "mail_id"] = "email99999"
-    df = encode(df, "mail_id")
-
-    df["bookingdate"] = pd.to_datetime(df["bookingdate"])
-
-    df.loc[df["cvcresponsecode"] > 2, "cvcresponsecode"] = 3
-    df["countries_equal"] = df["shoppercountrycode"] == df["issuercountrycode"]
-    df.loc[df["countries_equal"] == False, "countries_equal"] = 0
-    df.loc[df["countries_equal"] == True, "countries_equal"] = 1
-
-    df["day_of_week"] = df["creationdate"].dt.dayofweek
-    df["hour"] = df["creationdate"].dt.hour
-
-
-    return df
-
-
-if __name__ == "__main__":
-    data = "./data/data_for_student_case.csv"
-    df = pd.read_csv(data)
-    preprocess(df)
