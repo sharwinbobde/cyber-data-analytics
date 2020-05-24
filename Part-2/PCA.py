e0e000c77344ea815abe11483f72e63b15271477
sharwinbobde <bsharwin@gmail.com>
2020-05-24 13:45:22 +0200

    PCA complete

--- Part-2/PCA.py
+++ Part-2/PCA.py
@@ -7,38 +7,41 @@ class PCA_Component:
     
     def set_X(self, X):
         self.X = X
-        max_ = np.max(X)
-        min_ = np.min(X)
-        X_normed = (X - min_)/(max_ - min_) # normalise
+        self.max_ = np.max(X)
+        self.min_ = np.min(X)
+        X_normed = (X - self.min_)/(self.max_ - self.min_) # normalise
         self.X_normed = X_normed
 
     def PCA_fit_transform(self, n_components):
         # new PCA started whenever we calculate a new encoding
         self.pca = PCA(n_components=n_components)
         encoding = self.pca.fit_transform(self.X_normed)
+
+        # keep residual max min correct
+        R = self.calculate_residual(self.X)
+        R = np.sum(R, axis=1) # sum up residuals for all signals
+        self.max_R = np.max(R)
+        self.min_R= np.min(R)
+
         return encoding
 
     def PCA_transform(self, Z):
-        max_ = np.max(Z)
-        min_ = np.min(Z)
-        Z_normed = (Z - min_)/(max_ - min_) # normalise
+        Z_normed = (Z - self.min_)/(self.max_ - self.min_) # normalise
         encoding = self.pca.fit_transform(Z_normed)
         return encoding
     
     def calculate_residual(self, Z):
-        max_ = np.max(Z)
-        min_ = np.min(Z)
-        Z_normed = (Z - min_)/(max_ - min_) # normalise
-
         encoding = self.PCA_transform(Z)
         Z_normed_inv = self.pca.inverse_transform(encoding)
 
+        Z_normed = (Z - self.min_)/(self.max_ - self.min_) # normalise
         return (Z_normed - Z_normed_inv)**2     # Return squared error
 
-    def anomaly_score(self, Z,):
+    def anomaly_score(self, Z):
         R = self.calculate_residual(Z)
         R = np.sum(R, axis=1) # sum up residuals for all signals
-        max_ = np.max(R)
-        min_ = np.min(R)
+        # use the best min and max
+        max_ = np.max([np.max(R), self.max_R]) 
+        min_ = np.min([np.min(R), self.min_R])
         scores = (R - min_)/(max_ - min_)
         return scores
