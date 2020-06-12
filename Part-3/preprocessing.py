24879f6f4bd0e99bee20429ef5a37e7a742014a8
Nikhil Saldanha <nikhil.saldanha691@gmail.com>
2020-06-12 14:05:25 +0200

    add all answers to questions

--- Part-3/preprocessing.py
+++ Part-3/preprocessing.py
@@ -46,3 +46,42 @@ def encode_labels(df):
     df.loc[:, "Label"] = enc.transform(df["Label"])
 
     return df
+
+def encode_features(df):
+    """
+    Numerically encode all the features
+    """
+
+    df.loc[df["Sport"].isna(), "Sport"] = "UNK"
+    sport_enc = LabelEncoder()
+    df.loc[:, "Sport"] = sport_enc.fit_transform(df["Sport"])
+
+    df.loc[df["Dport"].isna(), "Dport"] = "UNK"
+    dport_enc = LabelEncoder()
+    df.loc[:, "Dport"] = dport_enc.fit_transform(df["Dport"])
+
+    df.loc[df["State"].isna(), "State"] = "UNK"
+    state_enc = LabelEncoder()
+    df.loc[:, "State"] = state_enc.fit_transform(df["State"])
+
+    df.loc[df["sTos"].isna(), "sTos"] = -999
+    stos_enc = LabelEncoder()
+    df.loc[:, "sTos"] = stos_enc.fit_transform(df["sTos"])
+
+    df.loc[df["dTos"].isna(), "dTos"] = -999
+    dtos_enc = LabelEncoder()
+    df.loc[:, "dTos"] = dtos_enc.fit_transform(df["dTos"])
+
+    proto_enc = LabelEncoder()
+    df.loc[:, "Proto"] = proto_enc.fit_transform(df["Proto"])
+
+    dir_enc = LabelEncoder()
+    df.loc[:, "Dir"] = dir_enc.fit_transform(df["Dir"])
+
+    srcaddr_enc = LabelEncoder()
+    df.loc[:, "SrcAddr"] = srcaddr_enc.fit_transform(df["SrcAddr"])
+
+    dstaddr_enc = LabelEncoder()
+    df.loc[:, "DstAddr"] = dstaddr_enc.fit_transform(df["DstAddr"])
+
+    return df
